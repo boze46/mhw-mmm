f30b8553cf642d72a68f2015584e86725cfae252
boze <boze3141@gmail.com>
2025-12-05 15:55:39 +0800

    chore: 初始化项目结构
    初始化 Monster Hunter World Mod Manager 项目
    - 配置 Tauri + Vue + VueUse + shadcn-vue + Tailwind 技术栈
    - 完善 .gitignore 配置

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+// import { ref } from "vue";
+import "@/style.css"
+
+import { Button } from '@/components/ui/button'
+
+</script>
+
+<template>
+  <div>
+    <Button>Click me</Button>
+  </div>
+</template>
+
+<style>
+</style>
